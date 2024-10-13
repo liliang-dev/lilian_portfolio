141363025521dce3f8c2fe11fdfb616429b038e5
GARRIDOLilian <71266915+GARRIDOLilian@users.noreply.github.com>
2024-10-13 18:35:45 +0200

    ImageStaticData

--- src/app/_components/Project.tsx
+++ src/app/_components/Project.tsx
@@ -1,9 +1,9 @@
 import { cn } from "@/lib/utils"
 import Link from "next/link"
 import { PropsWithChildren } from "react"
-import Image from 'next/image'
+import Image, { StaticImageData } from 'next/image'
 
-export const Project = (props: PropsWithChildren<{className?:string, title:string, link:string, image:any}>) =>  {
+export const Project = (props: PropsWithChildren<{className?:string, title:string, link:string, image:StaticImageData}>) =>  {
     return (
         <Link href={props.link} target="_blank" className={cn("grid grid-cols-12 gap-5 font-grand p-5 rounded-lg duration-300 bg-[#f9f9f9] hover:bg-[#f1f1f1] hover:scale-105 text-foreground", props.className)}>
             <div className={`col-span-3 text-xs justify-center items-center flex flex-col`}>
