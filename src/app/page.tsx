3ea96e642a1eaa7ab7a013030836b974c0f91ff0
GARRIDOLilian <71266915+GARRIDOLilian@users.noreply.github.com>
2024-10-13 15:02:57 +0200

    Responsive Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,14 +11,14 @@ export default function Home() {
   return (
     <main className="mx-auto min-h-screen max-w-screen-xl font-pen">
       <div className="lg:flex lg:justify-center">
-        <Column className="lg:sticky lg:top-0 lg:flex lg:justify-between p-16 lg:max-h-screen lg:w-1/2">
-          <Section className="lg:flex lg:flex-col lg:justify-between lg:max-h-screen lg:px-10 max-sm:text-center">
+        <Column className="lg:sticky lg:top-0 lg:flex lg:justify-between py-16 px-10 lg:px-24 lg:max-h-screen lg:w-1/2">
+          <Section className="lg:flex lg:flex-col lg:justify-between lg:max-h-screen max-sm:text-center">
             <Profile/>
             <MainMenu className="flex flex-col text-3xl gap-3 py-10"/>
             <Contacts className="flex flex-row max-sm:justify-center gap-2"/>
           </Section>
         </Column>
-        <Column className="flex-1 h-full lg:w-1/2 p-16 lg:px-10">
+        <Column className="flex-1 h-full lg:w-1/2 py-16 px-10">
           <Section className="flex flex-col">
             <Experiences/>
             <Skills className="py-10"/>
