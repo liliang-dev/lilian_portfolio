7f01c8e525a6fa27936c6cbbd8bdb2b8a6cd3289
GARRIDOLilian <71266915+GARRIDOLilian@users.noreply.github.com>
2025-02-26 18:11:59 +0100

    Fix data reload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,24 +9,39 @@ import { SkillObject } from "./_components/Skill";
 import { ExperienceObject } from "./_components/Experience";
 import { sql } from '@vercel/postgres';
 
-export default async function HomePage() {
-  const experiences = await sql<ExperienceObject>`select * from experiences order by from_year desc limit 3;`;
-  const skills = await sql<SkillObject>`select * from skills;`;
-  const projects = await sql<ProjectObject>`SELECT p.*, COALESCE(JSON_AGG(JSON_BUILD_OBJECT('id', s.id,'color', s.color,'name', s.name)) FILTER (WHERE s.id IS NOT NULL), '[]') AS skills
-                                            FROM projects p
-                                            LEFT JOIN project_skills ps ON ps.project_id = p.id
-                                            LEFT JOIN skills s ON ps.skill_id = s.id
-                                            GROUP BY p.id
-                                            ORDER BY p.sort_order
-                                            LIMIT 3;`;
-  const studies = await sql<StudyObject>`select * from studies order by from_year desc limit 4;`;
+// Get des datas nécéssaire dans la page d'accueil
+async function getData() {
+  const [experiences, skills, projects, studies] = await Promise.all([
+    sql<ExperienceObject>`select * from experiences order by from_year desc limit 3;`,
+    sql<SkillObject>`select * from skills;`,
+    sql<ProjectObject>`SELECT p.*, COALESCE(JSON_AGG(JSON_BUILD_OBJECT('id', s.id,'color', s.color,'name', s.name)) FILTER (WHERE s.id IS NOT NULL), '[]') AS skills
+                                              FROM projects p
+                                              LEFT JOIN project_skills ps ON ps.project_id = p.id
+                                              LEFT JOIN skills s ON ps.skill_id = s.id
+                                              GROUP BY p.id
+                                              ORDER BY p.sort_order
+                                              LIMIT 3;`,
+    sql<StudyObject>`select * from studies order by from_year desc limit 4;`
+  ]);
 
+  return {
+    experiences: experiences.rows, 
+    skills: skills.rows, 
+    projects: projects.rows, 
+    studies: studies.rows
+  };
+}
+
+// Affichage de la page d'accueil
+export default async function HomePage() {
+  const { experiences, skills, projects, studies } = await getData();
+  
   return (
     <>
-      <Experiences experiences={experiences.rows} isPage={false} />
-      <Skills skills={skills.rows} />
-      <Projects projects={projects.rows} isPage={false} />
-      <Studies studies={studies.rows} />
+      <Experiences experiences={experiences} isPage={false} />
+      <Skills skills={skills} />
+      <Projects projects={projects} isPage={false} />
+      <Studies studies={studies} />
       <Footer />
     </>
   );
